f5cfeba7c23c13c264bef80ca07a49684985d6d2
Phạm Hoài An <phamhoaianctu@gmail.com>
2017-11-22 05:00:27 +0700

    Tao lai project
    add the  thu vien Firebase,
    tao san main voi bottom navigation

---
+++ app/src/main/java/com/example/phamh/devfest/Service/APIService.java
@@ -0,0 +1,15 @@
+package com.example.phamh.devfest.Service;
+
+/**
+ * Created by phamh on 11/22/2017.
+ * Đây là class thông tin về api Firebase
+ * Tài khoản google: devfestgdgmekong@gmail.com
+ * mật khẩu: devfest123456789
+ */
+
+public class APIService {
+    /**
+     * Firebase API KEY
+     * */
+    public static String FIREBASE_KEY = "";
+}
